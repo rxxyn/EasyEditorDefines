052cc499dbc231d5fe3302b121972565e7886565
rxxyn <113850083+rxxyn@users.noreply.github.com>
2023-08-04 22:48:47 -0700

    Add files via upload

--- EasyEditorDefines.cs
+++ EasyEditorDefines.cs
@@ -8,6 +8,9 @@ namespace EasyEditorDefines
         /// </summary>
         public static bool IsUnityEditor = true;
 #else
+        /// <summary>
+        /// Are you on the Unity Editor?
+        /// </summary>
         public static bool IsUnityEditor = false;
 #endif
 
@@ -17,7 +20,34 @@ namespace EasyEditorDefines
         /// </summary>
         public static bool IsUnityEditor_Win = true;
 #else
+        /// <summary>
+        /// Is your Unity Editor platform Windows?
+        /// </summary>
         public static bool IsUnityEditor_Win = false;
+#endif
+
+#if UNITY_EDITOR_OSX
+        /// <summary>
+        /// Is your Unity Editor platform Windows?
+        /// </summary>
+        public static bool IsUnityEditor_Mac = true;
+#else
+        /// <summary>
+        /// Is your Unity Editor platform Windows?
+        /// </summary>
+        public static bool IsUnityEditor_Mac = false;
+#endif
+
+#if UNITY_EDITOR_LINUX
+        /// <summary>
+        /// Is your Unity Editor platform Windows?
+        /// </summary>
+        public static bool IsUnityEditor_Linux = true;
+#else
+        /// <summary>
+        /// Is your Unity Editor platform Windows?
+        /// </summary>
+        public static bool IsUnityEditor_Linux = false;
 #endif
 
 #if UNITY_ANDROID
@@ -26,10 +56,16 @@ namespace EasyEditorDefines
         /// </summary>
         public static bool IsUnityGameAndroid = true;
 #else
+        /// <summary>
+        /// Is your Build Platform set to Android?
+        /// </summary>
         public static bool IsUnityGameAndroid = false;
 #endif
 
 #if UNITY_BUILD_64
+        /// <summary>
+        /// Is your Unity Build x64?
+        /// </summary>
         public static bool IsUnityBuild64 = true;
 #else
         /// <summary>
@@ -38,5 +74,76 @@ namespace EasyEditorDefines
         public static bool IsUnityBuild64 = false;
 #endif
 
+#if UNITY_SERVER
+        /// <summary>
+        /// Is the Unity Project in Server mode? (Normally used for Mirror)
+        /// </summary>
+        public static bool IsUnityServer = true;
+#else
+        /// <summary>
+        /// Is the Unity Project in Server mode? (Normally used for Mirror)
+        /// </summary>
+        public static bool IsUnityServer = false;
+#endif
+
+#if UNITY_IOS
+         /// <summary>
+         /// Are you playing this Unity game on the IOS platform?
+         /// </summary>
+         public static bool IsUnityGameIOS = true;
+#else
+        /// <summary>
+        /// Are you playing this Unity game on the IOS platform?
+        /// </summary>
+        public static bool IsUnityGameIOS = false;
+#endif
+
+#if UNITY_STANDALONE_OSX
+        /// <summary>
+        /// Are you playing this Unity game on the OSX platform?
+        /// </summary>
+         public static bool IsUnityGameOSX = true;
+#else
+        /// <summary>
+        /// Are you playing this Unity game on the OSX platform?
+        /// </summary>
+        public static bool IsUnityGameOSX = false;
+#endif
+
+#if UNITY_STANDALONE_WIN
+        /// <summary>
+        /// Are you playing this Unity game on the Windows platform?
+        /// </summary>
+         public static bool IsUnityGameWin = true;
+#else
+        /// <summary>
+        /// Are you playing this Unity game on the Windows platform?
+        /// </summary>
+        public static bool IsUnityGameWin = false;
+#endif
+
+#if UNITY_TVOS
+        /// <summary>
+        /// Are you playing this Unity game on the Apple TVOS platform?
+        /// </summary>
+         public static bool IsUnityGameTVOS = true;
+#else
+        /// <summary>
+        /// Are you playing this Unity game on the Apple TVOS platform?
+        /// </summary>
+        public static bool IsUnityGameTVOS = false;
+#endif
+
+#if UNITY_STANDALONE
+        /// <summary>
+        /// Are you playing this Unity game? (Any Platform)
+        /// </summary>
+         public static bool IsUnityGame = true;
+#else
+        /// <summary>
+        /// Are you playing this Unity game? (Any Platform)
+        /// </summary>
+        public static bool IsUnityGame = false;
+#endif
     }
 }
